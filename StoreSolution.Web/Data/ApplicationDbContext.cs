348037f79869bafba10139ba9977180b05c7a3b8
andresmgomez <andresmgomez23@gmail.com>
2023-08-07 13:58:59 -0500

    Add entity framework core as a service

---
+++ StoreSolution.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreSolution.Web.Models;
+
+namespace StoreSolution.Web.Data
+{
+    public class ApplicationDbContext: DbContext 
+    {
+        /// <summary>
+        /// Make an instance of database context and pass it to the application
+        /// </summary>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        // Code first approach for creating an SQL table
+        public DbSet<DepartmentModel> Departments { get; set; }
+    }
+}
--- StoreSolution.Web/Program.cs
+++ StoreSolution.Web/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using StoreSolution.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add service to connect to SQL Database
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
 // Add Hot Reload service to all pages
 builder.Services.AddRazorPages();
 
