dea1df74500a1968276089fc050590ae371b74ac
andresmgomez <andresmgomez23@gmail.com>
2023-08-07 18:11:40 -0500

    Add first migration to create table

---
+++ StoreSolution.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreSolution.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
