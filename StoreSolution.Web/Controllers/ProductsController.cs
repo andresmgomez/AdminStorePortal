34143651edf1375bf021783b8b4df9c2e23f835f
andresmgomez <andresmgomez23@gmail.com>
2023-08-09 15:54:20 -0500

    Display example of store products

---
+++ StoreSolution.Web/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreSolution.Web.Data;
+using StoreSolution.Web.Models;
+using System.Collections.Generic;
+
+namespace StoreSolution.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _dataContext;
+
+        public ProductsController(ApplicationDbContext dataContext)
+        {
+            _dataContext = dataContext;   
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product>currentProducts = _dataContext.StoreProducts.ToList();
+            return View(currentProducts);
+        }
+    }
+}
